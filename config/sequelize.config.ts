5867c6ed31843a073db87385336f9fdfedacb6de
Dmitriy <dmitriytuz123@gmail.com>
2023-05-25 00:35:19 +0300

    tests for controller and service for addTagWithTransaction is ready

---
+++ config/sequelize.config.ts
@@ -0,0 +1,18 @@
+import { SequelizeOptions } from 'sequelize-typescript';
+import {User} from "../src/users/users.model";
+import {Tag} from "../src/tags/tags.model";
+import {UserTags} from "../src/tags/user-tags.model";
+
+const sequelizeConfig: SequelizeOptions = {
+    database: 'RaDevs-db-test',
+    username: 'postgres',
+    password: '1111',
+    host: 'localhost',
+    port: 5432, // порт базы данных
+    dialect: 'postgres', // используемая СУБД (например, PostgreSQL)
+    models: [User, Tag, UserTags], // массив моделей, которые Sequelize должен знать
+    logging: false, // отключение логирования SQL-запросов (по желанию)
+
+};
+
+export default sequelizeConfig;
