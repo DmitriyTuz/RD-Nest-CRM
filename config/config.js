a5006e81751f70fbe3c92e5e99e4c521225764c5
Dmitriy <dmitriytuz123@gmail.com>
2023-07-15 15:01:29 +0300

    everything works with docker 1

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
-const {POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PROTOCOL, POSTGRES_PORT} = require('../credentials').config;
+// console.log('process.env.NODE_ENV = ', process.env.NODE_ENV);
+// if (process.env.NODE_ENV === 'development') {
+      require('dotenv').config({ path: '.development.env' });
+// }
 
 module.exports =
     {
