23126950d0363cee157392435c1ebb891ea0925e
Dmitriy <dmitriytuz123@gmail.com>
2023-07-21 16:51:35 +0300

    tests and docker works

---
+++ src/firebase.config/rd-nest-crm3-firebase-adminsdk-qidrt-0b350ef917.ts
@@ -0,0 +1,18 @@
+import sequelizeConfig from "../../config/sequelize.config";
+
+const pathPrivateKeyJs =
+    {
+      type: "service_account",
+      project_id: "rd-nest-crm3",
+      private_key_id: "0b350ef917693eeeb40cc04baa9f5b340eb69e86",
+      private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDlPCGL9w+gV6im\n02MO4q+3P8IuoxGNCG/OF/dPAVTVS2AoI93hYNG2rle/fgiu9MeSJrWobij0U1UN\njp1Sfl6AF3mo1FflY95bD7R6Xu1tLtgjLRlSfeSpXIuJLrDb13Mk2AMLXgeWy8QV\nxt492UldQwWsGztNHt01JH7nFW96T1E5Q0hMwFeSrlEWeoLL+XY3lPMc4dPhvcb9\n3J3mMhkWjA1uCvJBlTII8i7t2thb7ohQBVyCG5GRF/VjtRP6x+LB5D2KoN0y6zZI\ndHcoHyzntPupbLOzQJKDXSBiaIo8eicjq/FKr7O8T/ICSACWj5OJzvEAYbnCmr/S\nViNHg6PdAgMBAAECggEACWXPb0RD6vwlAR/Z6AaRkxblUFGlms1Nfhq77Ax/LJZg\nPQ0JX9+/ATkBBmv1kayd0OdO27aWbbvL2RgvaW4eVCyCM5Ju/wu61itc6AZjv+3j\ndP+4kcnw+aN+YfOc0wpwYUKMz2a9s2grpAPo4Qq7J/xjK2TbZ8Q7592U6UbW8OHj\nL+0vFu7J2rRxyl4BgjgdFKZ3qxv1/Ro3adDcZ04BO+Glhkyf2o64EdO658zd4DZE\nuSGf5YLJ24TgaeRqx4DY5h+xBdvtiJHo7MLBpA1RnQT9imG9pNgSkvVQjeIXgfSp\nZKLr3Gp/O06lshlucKP648R4MkEJoQopVU23PNx94QKBgQD6xt+T7ATSaHoRxzTK\nub9dtFIXaz2scGqyszc9+UpKeez6zBMgXlIrzLsdnfxulJYBJch5XBdb/C+Y/7NA\nSORqHxK3asZevp81z+Ur7GgHOLgaNF8gt6Q0VbKKgVVbXzf8iBWyr2NC6ySqlb66\nm+yDYDsGCI7vljgRdlPDNNEw/QKBgQDqAmW0OUwVeH9s8a1BU++zAwfAsqc3tMPS\nF09u0kJMQKM5/U5MQrpUYt8HQb227RZ0sJOjp1g8stPt4pUFm0to6suyndJPVlwa\n5UJ7WGY2fhMBGmmjaTZsa5AYhC869euj/jKy0au1grEUO9vO2GoU1NyArtzIHhDA\nFdRBMbakYQKBgQCC4ps8XBnkK21SrBLsn+e2I3AZEvq8bmvx2sHBUcQpfNtnxm1R\nHolqnM8oVRW0LU7hSHQKizsR4lyBUSugKKs2jAM7wbnJJ1p3PotQ0nE1G7BS1PS1\nYcONIREvYvNG3jqdTfVtLkbzXUDk24URZmIZ63bC3mKjv1aCHqz3HDD2jQKBgF9A\nIo29VM7YgWWi6dbRh5vh++Hf1ljCZ4K4okiFpR7MoKQWCyzXbGi05ftaMxKmZqGQ\nzkeq8iuizsjhla72izdlewx1WQmaKVt5Phpe3RwJLUshqSYuyUxQbBLOmzrwpr3V\n1mUbGz2CuPl3c/058dQQ4MAwbQW31zGYsRDoX2OBAoGBANWWKqHjXTxLNoZAllMP\nfdVDLRdLuQhALbbLZC15ynGMIwDMkXSmna/qQ+i3ma6WIgJ8rFIk4hzxUyvPKC4o\nLqNkxBEQ1GMpaaDWW6YS2U4LEdRf0Vk+xXCnlqZq+UWImQBcF2gjsGLbA6sXadGo\nA0g28IOUj1xIWFnmxKy147Sr\n-----END PRIVATE KEY-----\n",
+      client_email: "firebase-adminsdk-qidrt@rd-nest-crm3.iam.gserviceaccount.com",
+      client_id: "101059850296048456738",
+      auth_uri: "https://accounts.google.com/o/oauth2/auth",
+      token_uri: "https://oauth2.googleapis.com/token",
+      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+      client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-qidrt%40rd-nest-crm3.iam.gserviceaccount.com",
+      universe_domain: "googleapis.com"
+    }
+
+export default pathPrivateKeyJs;
